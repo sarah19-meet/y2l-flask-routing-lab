7a6dffeec8e9fdcfb7e31f360d4e3c66bced3d6e
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:47:30 +0300

    web

--- app.py
+++ app.py
@@ -3,7 +3,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+    return render_template("home.html")
+
+@app.route('/shop')
+def home():
+    return render_template("shop.html")
+
+@app.route('/list')
+def list1():
+    colors=["red","grey","yellow"]
+    return render_template("hello_Vars.html", colors=colors)
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
